4b74f9b099f411d1e4be3f1563092aa941437248
Jaap Roes <jaap.roes@gmail.com>
2022-08-23 12:35:11 +0200

    Use splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import { resolve } from "path"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
@@ -15,14 +15,5 @@ export default defineConfig({
   },
   preview: {
     host: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["vue", "pinia"],
-        },
-      },
-    },
   },
 })
