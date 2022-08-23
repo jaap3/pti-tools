803344a64651407326de608cb3454e04c91f6288
Jaap Roes <jaap.roes@gmail.com>
2022-08-24 00:27:42 +0200

    Create a simple vite plugin to use EJS in index.html
    Use it to replace base_url in og:meta / twitter card tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
+import { join as pathJoin } from "path"
+import * as ejs from "ejs"
+import type { Plugin, ResolvedConfig } from "vite"
 import { defineConfig, splitVendorChunkPlugin } from "vite"
 import { resolve } from "path"
 import vue from "@vitejs/plugin-vue"
 
+const htmlPlugin = (): Plugin => {
+  let config: ResolvedConfig | undefined
+  return {
+    name: "vite-plugin-ejs-html",
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
+    transformIndexHtml(html) {
+      return ejs.render(html, { ...config.env, path: { join: pathJoin } })
+    },
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), splitVendorChunkPlugin()],
+  plugins: [vue(), splitVendorChunkPlugin(), htmlPlugin()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
