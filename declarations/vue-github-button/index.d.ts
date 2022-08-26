f8996a7491b7cacb8b128599f5bfdbe6651bd03d
Jaap Roes <jaap.roes@gmail.com>
2022-08-26 02:43:39 +0200

    Workaround broken index.d.ts in vue-github-bottons

---
+++ declarations/vue-github-button/index.d.ts
@@ -0,0 +1,11 @@
+declare const GithubButton: import("vue").DefineComponent<{
+  href?: string
+  ariaLabel?: string
+  title?: string
+  dataIcon?: string
+  dataColorScheme?: string
+  dataSize?: string
+  dataShowCount?: string
+  dataText?: string
+}>
+export default GithubButton
