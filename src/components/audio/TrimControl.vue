f7137eae36bb242657dffad5243f820b6144887d
Jaap Roes <jaap.roes@gmail.com>
2023-04-16 17:49:08 +0200

    Update stylelint, lintfix

--- src/components/audio/TrimControl.vue
+++ src/components/audio/TrimControl.vue
@@ -57,10 +57,7 @@
   input,
   input:checked {
     position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
+    inset: 0;
     appearance: none;
     cursor: pointer;
     border: none;
